39a1909e6ac1fe448ef90e4c442cf742d8d4a7ee
Ghcat0528 <fcatalanog7@gmail.com>
2024-10-13 22:11:46 -0600

    finished assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+//MULTIPLICATION
+// test expects two numbers to return the product of those numbers
+//expect product of x,x to equal sum
+
+//test expects two numbers to not return the product of those numbers
+//expect product of x,x to not equal sum
+
+//test expects that not two numbers to not return the product of those numbers
+//expect product of x,z to not equal sum
+
+//test expects that more than two numbers to not return the product of those numbers
+//expect produce of x,x,x to not equal sum
+
+//test expects that less than two numbers to not return the product of those numbers
+//expect produce of x to not equal sum
+
+//CONCATODDS
+//test expects when given two arrays of integers to return single array only containing odd numbers in ascending order from both arrays
+//expects a single array of integers containing odd numbers in ascending order using integers from both arrays
+
+//test expects when given one array of integers to return single array only containing odd numbers in ascending order from both arrays
+//expects a single array of integers containing odd numbers in ascending order to not use integers from two arrays
+
+//test expects when given one array of integers to return single array not containing odd numbers in ascending order from both arrays
+//expects a single array of integers to not contain odd numbers in ascending order using integers from both arrays
+
+//test expects when given two arrays of integers to return single array only containing odd numbers in ascending order from not both arrays
+//expects a single array of integers containing odd numbers in ascending order using integers from not both arrays
+
+//test expects when there are no arrays of integers to return no array only containing odd numbers in ascending order from no arrays
+//expects a no array of integers containing odd numbers in ascending order using integers from both arrays
